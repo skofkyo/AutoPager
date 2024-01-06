b8c25619717c28c953daa2f4429617e03da31ebb
skofkyo <kyo7350201@gmail.com>
2024-01-06 17:05:14 +0800

    Update FullPictureLoad.user.js

--- CustomPictureDownload/FullPictureLoad.user.js
+++ CustomPictureDownload/FullPictureLoad.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load - FancyboxV5
 // @name:zh-CN         图片全载-FancyboxV5
 // @name:zh-TW         圖片全載-FancyboxV5
-// @version            1.8.8
+// @version            1.8.9
 // @description        專注於寫真、H漫、漫畫的網站，目前規則數600+，進行圖片全量加載，讓你免去需要翻頁的動作，也能進行下載壓縮打包，如有下一頁元素能做到自動化下載。
 // @description:en     Load all pictures for picture websites, and can also compress and package them for download.
 // @description:zh-CN  专注于写真、H漫、漫画的网站，目前规则数600+，进行图片全量加载，也能进行下载压缩打包，如有下一页元素能做到自动化下载。
@@ -13139,6 +13139,22 @@ document.body.appendChild(text);
             //Search單列顯示，透過腳本管理器選單開啟。
             if (/\/search\//.test(window.location.pathname) && lazyLoadSingleColumn == 1) {
                 fun.css(".mantine-xhyubp{display:block}");
+                setInterval(() => {
+                    if (/\/search\//.test(window.location.pathname)) {
+                        let parentWidth = document.querySelector(".mantine-fui8ih").offsetWidth;
+                        [...document.querySelectorAll(".mantine-wc580l")].forEach(div => {
+                            let img = div.querySelector("img.__mantine-ref-image");
+                            if (img) {
+                                if (img.naturalWidth < parentWidth) {
+                                    div.style.height = img.height + "px";
+                                } else {
+                                    let num = parentWidth / img.naturalWidth;
+                                    div.style.height = parseInt(img.naturalHeight * num, 10) + "px";
+                                }
+                            }
+                        });
+                    }
+                }, 1000);
             }
             //Tags單列顯示，透過腳本管理器選單開啟。
             if (/\?tags=/.test(window.location.search) && lazyLoadSingleColumn == 1) {
