263fffedf90c21cdad271ff3c487013f4c623061
skofkyo <kyo7350201@gmail.com>
2024-08-10 00:04:48 +0800

    Update FullPictureLoad.user.js

--- CustomPictureDownload/FullPictureLoad.user.js
+++ CustomPictureDownload/FullPictureLoad.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load - FancyboxV5
 // @name:zh-CN         图片全载-FancyboxV5
 // @name:zh-TW         圖片全載-FancyboxV5
-// @version            2.5.4
+// @version            2.5.5
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully loaded images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
@@ -6598,7 +6598,7 @@ return [...matchObj].map(arr => arr[1].replaceAll("\\u002F", "/"));
         name: "エロ画像まとめ",
         host: ["geinou-nude.com"],
         reg: /^https?:\/\/geinou-nude\.com\/[^\/]+\/(#.*)?$/,
-        imgs: ".post_content a[href*='/uploads/']",
+        imgs: ".post_thum>img,.post_content a[href*='/uploads/']",
         autoDownload: [0],
         next: "a.nav_link_l",
         prev: "a.f_row_r",
@@ -24525,7 +24525,6 @@ img.default {
     height: auto;
     max-width: 98vw;
     max-height: 99vh;
-    min-height: 99vh;
     border: solid #fff;
 }
 img.single {
@@ -24794,6 +24793,7 @@ function createImgElement(mode) {
     imgViewIndex = -1;
     [...document.querySelectorAll(".FixedMenuitem")].forEach(item => item.classList.remove("active"));
     document.querySelector("#imgBox").innerHTML = "";
+    const verticalScreen = window.innerHeight > window.innerWidth;
     const imgElements = newImgs.map((src, i, arr) => {
         let a = document.createElement("a");
         a.href = src;
@@ -24803,6 +24803,11 @@ function createImgElement(mode) {
         img.className = mode;
         img.src = "${loading_bak}";
         img.dataset.src = src;
+        if (verticalScreen && mode === "default") {
+            img.style.minWidth = "98vw";
+        } else if (!verticalScreen && mode === "default") {
+            img.style.minHeight = "99vh";
+        }
         a.appendChild(img);
         return a;
     });
@@ -25020,11 +25025,17 @@ function createImgElement(mode) {
     imgViewIndex = -1;
     [...document.querySelectorAll(".FixedMenuitem")].forEach(item => item.classList.remove("active"));
     document.querySelector("#imgBox").innerHTML = "";
+    const verticalScreen = window.innerHeight > window.innerWidth;
     const imgElements = newImgs.map((src, i, arr) => {
         let img = document.createElement("img");
         img.className = mode;
         img.src = "${loading_bak}";
         img.dataset.src = src;
+        if (verticalScreen && mode === "default") {
+            img.style.minWidth = "98vw";
+        } else if (!verticalScreen && mode === "default") {
+            img.style.minHeight = "99vh";
+        }
         return img;
     });
     document.querySelector("#imgBox").append(...imgElements);
