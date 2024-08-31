145ee3ee949778801b7ee08171101a57d005642a
skofkyo <kyo7350201@gmail.com>
2024-08-31 16:43:59 +0800

    Update FullPictureLoad.user.js

--- CustomPictureDownload/FullPictureLoad.user.js
+++ CustomPictureDownload/FullPictureLoad.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load - FancyboxV5
 // @name:zh-CN         图片全载-FancyboxV5
 // @name:zh-TW         圖片全載-FancyboxV5
-// @version            2.7.8
+// @version            2.7.9
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully loaded images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
@@ -19204,8 +19204,6 @@ if (next) {
         reg: /^https?:\/\/(www\.)?(copymanga\.site|copymanga\.tv|mangacopy\.com)\/comic\/\w+$/,
         delay: 300,
         init: async () => {
-            fun.clearAllTimer(3);
-            if ("aboutBlank" in _unsafeWindow) _unsafeWindow.aboutBlank = null;
             await fun.waitEle(".tab-pane.show.active a");
             const updateLastChapter = () => {
                 let [, , comic] = fun.lp.split("/");
@@ -19242,8 +19240,10 @@ if (next) {
             };
             updateLastChapter();
             document.addEventListener("visibilitychange", updateLastChapter);
+            if ("aboutBlank" in _unsafeWindow) _unsafeWindow.aboutBlank = null;
+            setTimeout(() => fun.clearAllTimer(3), 1000);
         },
-        css: ".lastchapter{color:#fff !important;background:#1790E6}",
+        css: ".lastchapter{color:#fff !important;background:#1790E6}.comicDetailAds{display:none!important;}",
         category: "none"
     }, {
         name: "拷貝漫畫 清除不給開啟開發人員工具",
