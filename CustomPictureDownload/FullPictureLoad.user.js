9a69321979d86cd39477fb62fad8b21b992ac46e
skofkyo <kyo7350201@gmail.com>
2024-09-12 11:57:19 +0800

    Update FullPictureLoad.user.js

--- CustomPictureDownload/FullPictureLoad.user.js
+++ CustomPictureDownload/FullPictureLoad.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load - FancyboxV5
 // @name:zh-CN         图片全载-FancyboxV5
 // @name:zh-TW         圖片全載-FancyboxV5
-// @version            2.7.19
+// @version            2.7.20
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully loaded images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
@@ -8309,6 +8309,41 @@ a:has(>div>div>img),
         }),
         css: "#page+div[id][class]{display:none!important;}",
         category: "ad"
+    }, {
+        name: "Nevsepic",
+        reg: () => fun.checkUrl({
+            h: "nevsepic",
+            e: [
+                "//div[@class='full-comms']/a[text()='18+']",
+                ".full-text img,a.highslide",
+                ".share_widget"
+            ]
+        }),
+        init: () => fun.createImgBox(".share_widget", 1),
+        imgs: () => fun.getImgA("a.highslide,.full-text img", ".navigation>a").then(srcs => srcs.filter(src => !src.includes("/thumbs/") && !src.includes("attach.png"))),
+        button: [4],
+        insertImg: [
+            ["#FullPictureLoadMainImgBox", 0, ".full-text img:not(.FullPictureLoadImage,[src$='attach.png']),.full-text img:not(.FullPictureLoadImage,[src$='attach.png'])~br,a.highslide,a.highslide~br,.bottom-nav"], 2
+        ],
+        customTitle: ".f-page-title",
+        category: "nsfw2"
+    }, {
+        name: "Nevsepic",
+        reg: () => fun.checkUrl({
+            h: "nevsepic",
+            e: [
+                ".full-text img,a.highslide",
+                ".share_widget"
+            ]
+        }),
+        imgs: () => {
+            let srcs = fun.getImgSrcArr(".full-text img,a.highslide");
+            return srcs.filter(src => !src.includes("/thumbs/") && !src.includes("attach.png"));
+        },
+        capture: () => _this.imgs(),
+        button: [4],
+        customTitle: ".f-page-title",
+        category: "nsfw2"
     }, {
         name: "DTF",
         reg: () => fun.checkUrl({
