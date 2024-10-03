8a375757ecd71d7bf2062fb8bb0ee2f4acda50c7
skofkyo <kyo7350201@gmail.com>
2024-10-03 15:05:14 +0800

    Update happymh_reading_aid.user.js

--- happymh_reading_aid.user.js
+++ happymh_reading_aid.user.js
@@ -3,7 +3,7 @@
 // @name:en            Happymh reading aid
 // @name:zh-CN         嗨皮漫画阅读辅助
 // @name:zh-TW         嗨皮漫畫閱讀輔助
-// @version            2.5.11
+// @version            2.5.12
 // @description        無限滾動模式(自動翻頁、瀑布流)，背景預讀圖片，自動重新載入出錯的圖片，左右方向鍵切換章節，目錄頁自動展開全部章節，新分頁打開漫畫鏈接。
 // @description:en     infinite scroll reading mode,Arrow keys to switch chapters,Background preload image,Auto reload image with error.
 // @description:zh-CN  无限滚动模式(自动翻页、瀑布流)，背景预读图片，自动重新加载出错的图片，左右方向键切换章节，目录页自动展开全部章节，新标籤页打开漫画链接。
@@ -502,7 +502,7 @@
     if (configs.arrowKey == 1 && isReadPage) {
         document.addEventListener("keydown", event => {
             if (ge("#mainHappymhConfigShadowElement")) return;
-            if (event.code === "ArrowRight") {
+            if (event.code === "ArrowRight" || event.key === "ArrowRight") {
                 const nextE = ge("//a[span[text()='下一话' or text()='下一話'] and starts-with(@href,'/reads/')]");
                 if (isString(nextChapterUrl)) {
                     _unsafeWindow.location.href = nextChapterUrl;
@@ -512,7 +512,7 @@
                     alert(i18n.tips.noNext);
                 }
             }
-            if (event.code === "ArrowLeft") {
+            if (event.code === "ArrowLeft" || event.key === "ArrowLeft") {
                 const prevE = ge("//a[span[text()='上一话' or text()='上一話'] and starts-with(@href,'/reads/')]");
                 if (isString(prevChapterUrl)) {
                     _unsafeWindow.location.href = prevChapterUrl;
