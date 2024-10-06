783be2eb136b39685abd6c11da14fa785b68d839
skofkyo <kyo7350201@gmail.com>
2024-10-06 10:07:02 +0800

    Update FullPictureLoad.user.js

--- CustomPictureDownload/FullPictureLoad.user.js
+++ CustomPictureDownload/FullPictureLoad.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load - FancyboxV5
 // @name:zh-CN         图片全载-FancyboxV5
 // @name:zh-TW         圖片全載-FancyboxV5
-// @version            2.8.29
+// @version            2.8.30
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully loaded images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
@@ -28103,12 +28103,14 @@ img.small {
             jumpSelect.addEventListener("change", () => {
                 config.jumpNum = jumpSelect.value;
                 saveConfig(config);
+                mainElement.focus();
             });
 
             behaviorInput.checked = config.behavior == "smooth" ? true : false;
             behaviorInput.addEventListener("change", () => {
                 config.behavior = behaviorInput.checked == true ? "smooth" : "instant";
                 saveConfig(config);
+                mainElement.focus();
             });
         }
         addFixedMenu();
@@ -28912,12 +28914,14 @@ img.small {
             jumpSelect.addEventListener("change", () => {
                 config.jumpNum = jumpSelect.value;
                 saveConfig(config);
+                mainElement.focus();
             });
 
             behaviorInput.checked = config.behavior == "smooth" ? true : false;
             behaviorInput.addEventListener("change", () => {
                 config.behavior = behaviorInput.checked == true ? "smooth" : "instant";
                 saveConfig(config);
+                mainElement.focus();
             });
         }
         addFixedMenu();
