bbb4a8587c3ff7cd35610e063f0d897f42c778b2
skofkyo <kyo7350201@gmail.com>
2024-10-20 14:22:21 +0800

    Update FullPictureLoad.user.js

--- CustomPictureDownload/FullPictureLoad.user.js
+++ CustomPictureDownload/FullPictureLoad.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load - FancyboxV5
 // @name:zh-CN         图片全载-FancyboxV5
 // @name:zh-TW         圖片全載-FancyboxV5
-// @version            2.9.4
+// @version            2.9.5
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully loaded images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
@@ -27867,7 +27867,15 @@ if (config.ViewMode == 1) {
             }
         };
         _unsafeWindow.addEventListener("keydown", kEvent);
-
+        /*
+        選單淡入淡出
+            transform: translate(0);
+            transition: all .8s ease-in-out;
+            &:hover {
+                transform: translate(138px);
+                transition: all .2s ease-in-out;
+            }
+        */
         fn.css(`
 html,body {
     overflow-y: hidden !important;
@@ -27896,11 +27904,8 @@ p#imgBox {
     border-radius: 3px;
     background-color: #fff;
     z-index: 2147483647;
-    transform: translate(0);
-    transition: all .8s ease-in-out;
     &:hover {
-        transform: translate(138px);
-        transition: all .2s ease-in-out;
+        left: 0px;
     }
 }
 .FixedMenuitem {
@@ -28803,11 +28808,8 @@ p#imgBox {
     border-radius: 3px;
     background-color: #fff;
     z-index: 2147483647;
-    transform: translate(0);
-    transition: all .8s ease-in-out;
     &:hover {
-        transform: translate(138px);
-        transition: all .2s ease-in-out;
+        left: 0px;
     }
 }
 .FixedMenuitem {
