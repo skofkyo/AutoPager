631e51e60261c4090855d4230576771829f425d0
skofkyo <kyo7350201@gmail.com>
2024-11-10 19:43:51 +0800

    Update FullPictureLoad.user.js

--- CustomPictureDownload/FullPictureLoad.user.js
+++ CustomPictureDownload/FullPictureLoad.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load - FancyboxV5
 // @name:zh-CN         图片全载-FancyboxV5
 // @name:zh-TW         圖片全載-FancyboxV5
-// @version            2.11.24
+// @version            2.11.25
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully loaded images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
@@ -10668,10 +10668,8 @@ a:has(>div>div>img),
         name: "JJGirls",
         url: {
             h: "jjgirls.com",
-            e: [
-                ".L664 a:has(>img:not([src^='/thumbs/']))",
-                ".L664 a>img:not([src^='/thumbs/'])"
-            ]
+            e: ".L664 a:has(>img:not([src^='/thumbs/']))",
+            d: "pc"
         },
         init: () => fn.createImgBox(".L664"),
         imgs: () => {
@@ -10687,23 +10685,11 @@ a:has(>div>div>img),
                     [, max] = link.match(/\/(\d+)\/$/);
                 }
                 let links = fn.arr(max, (v, i) => `${url}/${(i + 1)}/`);
-                return fn.getImgA(".L664 a:has(>img:not([src^='/thumbs/']))", links);
+                return fn.getImgA(".L664 a:has(>img:not([src^='/thumbs/']))", links, 1);
             } else {
-                return fn.gae(".L664 a:has(>img:not([src^='/thumbs/']))");
+                return fn.getImgA(".L664 a:has(>img:not([src^='/thumbs/']))", [fn.url]);
             }
         },
-        button: [4],
-        insertImg: ["#FullPictureLoadMainImgBox", 2],
-        go: 1,
-        category: "nsfw2"
-    }, {
-        name: "JJGirls M",
-        url: {
-            h: "jjgirls.com",
-            d: "m"
-        },
-        init: () => fn.createImgBox("div.p222308", 2),
-        imgs: "div.p222308>a:has(>img:not([src^='/temp'])):has(>img:not([src^='/thumbs']))",
         button: [4],
         insertImg: ["#FullPictureLoadMainImgBox", 2],
         go: 1,
@@ -10728,12 +10714,10 @@ a:has(>div>div>img),
                 "japanesethumbs.com",
                 "asiauncensored.com"
             ],
-            e: [
-                ".L664 a:has(>img:not([src^='/thumbs/']))",
-                ".L664 a>img:not([src^='/thumbs/'])"
-            ]
+            e: ".L664 a:has(>img:not([src^='/thumbs/']))",
+            d: "pc"
         },
-        init: () => fn.createImgBox(".L664"),
+        init: () => fn.createImgBox(".L664,.L996"),
         imgs: () => {
             let pagesE = fn.ge(".matchlinks");
             let pages = /\/\d+\/$/.test(fn.lp);
@@ -10748,9 +10732,9 @@ a:has(>div>div>img),
                     max = fn.gt(".matchlinks>a:last-child", 2);
                 }
                 let links = fn.arr(max, (v, i) => `${url}/${(i + 1)}/`);
-                return fn.getImgA(".L664 a:has(>img:not([src^='/thumbs/']))", links);
+                return fn.getImgA(".L664 a:has(>img:not([src^='/thumbs/']))", links, 1);
             } else {
-                return fn.gae(".L664 a:has(>img:not([src^='/thumbs/']))");
+                return fn.getImgA(".L664 a:has(>img:not([src^='/thumbs/']))", [fn.url]);
             }
         },
         button: [4],
@@ -33277,6 +33261,9 @@ a[data-fancybox-download]:active {
 
 a[data-fancybox]:hover {
     opacity: 1 !important;
+}
+.viewer-toolbar>ul>li {
+    line-height: unset !important;
 }
                 `;
 
