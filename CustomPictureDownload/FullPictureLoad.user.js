0ac87d4c1d2c897efc231cdf4401da81efe74d55
skofkyo <kyo7350201@gmail.com>
2024-11-25 00:03:14 +0800

    Update FullPictureLoad.user.js

--- CustomPictureDownload/FullPictureLoad.user.js
+++ CustomPictureDownload/FullPictureLoad.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load - FancyboxV5
 // @name:zh-CN         图片全载-FancyboxV5
 // @name:zh-TW         圖片全載-FancyboxV5
-// @version            2.11.33
+// @version            2.11.34
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully loaded images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
@@ -17288,7 +17288,7 @@ a:has(>div>div>img),
         exclude: ".captcha-area",
         fetchJson: (url = siteUrl) => {
             let [, , mangaCode, id] = new URL(url).pathname.split("/");
-            let api = `/v2.0/apis/manga/read?code=${mangaCode}&cid=${id}&v=v3.1302723`;
+            let api = `/v2.0/apis/manga/reading?code=${mangaCode}&cid=${id}&v=v3.1613134`;
             return fetch(api, {
                 "headers": {
                     "accept": "application/json, text/plain, */*",
