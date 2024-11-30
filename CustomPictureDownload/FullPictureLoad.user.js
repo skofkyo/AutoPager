a6202ed4c5ac27926b770ef6676d512e9a14fb08
skofkyo <kyo7350201@gmail.com>
2024-11-30 23:43:47 +0800

    Update FullPictureLoad.user.js

--- CustomPictureDownload/FullPictureLoad.user.js
+++ CustomPictureDownload/FullPictureLoad.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load - FancyboxV5
 // @name:zh-CN         图片全载-FancyboxV5
 // @name:zh-TW         圖片全載-FancyboxV5
-// @version            2.11.40
+// @version            2.11.41
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully loaded images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
@@ -715,7 +715,7 @@ a:has(>div>div>img),
         init: () => fn.createImgBox(".main", 1),
         url: {
             h: [
-                "www.xinwenba.net",
+                /\.xinwenba\.net$/,
                 /\.xwbar\.com$/,
                 /\.dv67\.com$/,
                 /\.xinent\.net$/,
@@ -28071,10 +28071,10 @@ if ("xx" in window) {
         return new Promise(resolve => {
             fetch(srcUrl, {
                 headers: {
-                    "accept": "*/*",
+                    "Accept": "*/*",
                     //"accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
                     //"cache-control": "no-cache",
-                    "upgrade-insecure-requests": "1"
+                    //"Upgrade-Insecure-Requests": "1"
                 },
                 referrer: getReferer(srcUrl),
                 referrerPolicy: "strict-origin-when-cross-origin",
@@ -28135,10 +28135,11 @@ if ("xx" in window) {
                 url: srcUrl,
                 responseType: "blob",
                 headers: {
-                    origin: fn.lo,
-                    referer: getReferer(srcUrl),
-                    accept: "*/*",
-                    "upgrade-insecure-requests": "1"
+                    "Origin": fn.lo,
+                    "Referer": getReferer(srcUrl),
+                    "User-Agent": navigator.userAgent,
+                    "Accept": "*/*",
+                    //"Upgrade-Insecure-Requests": "1"
                 },
                 cookie: getCookie(),
                 onload: async data => {
