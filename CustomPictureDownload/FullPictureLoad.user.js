93d4947cc1088a6f2dfed0b14acf43d5c017bc13
skofkyo <kyo7350201@gmail.com>
2024-12-06 21:53:18 +0800

    Update FullPictureLoad.user.js

--- CustomPictureDownload/FullPictureLoad.user.js
+++ CustomPictureDownload/FullPictureLoad.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load - FancyboxV5
 // @name:zh-CN         图片全载-FancyboxV5
 // @name:zh-TW         圖片全載-FancyboxV5
-// @version            2.11.44
+// @version            2.11.45
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully loaded images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
@@ -6107,7 +6107,8 @@ a:has(>div>div>img),
         name: "EroAsian",
         url: {
             h: "eroasian.net",
-            p: /^\/photo-set\//
+            p: "/photo-set/",
+            e: ".cm-entry-summary img"
         },
         init: () => {
             let info = fn.ge(".wp-block-group");
@@ -6404,14 +6405,15 @@ a:has(>div>div>img),
         category: "autoPager"
     }, {
         name: "4KHD",
-        host: ["www.4khd.com", "cfntz.xxtt.info", "ckdfv.xxtt.info", "svqds.xxtt.info"],
+        host: ["www.4khd.com", "uhpxi.xxtt.info", "zz.xxtt.info"],
         url: {
             e: "//a[@rel='home'][text()='4KHD']",
             p: /^\/content\/\d+\/[^\.\/]+\.html$/
         },
-        imgs: async () => {
-            thumbnailSrcArray = await fn.getImgA("figure.wp-block-image>a>img,#basicExample>a>img,.entry-content>p>a>img", ".page-link-box a").then(arr => arr.map(e => e.replace(/\?w=\d+$/, "?w=100")));
-            let bigSrcArray = thumbnailSrcArray.map(e => e.replace(/\/w\d+-rw\//, "/w7680-rw/").replace("?w=100", ""));
+        imgs: () => fn.getImgA("figure.wp-block-image>a>img,#basicExample>a>img,.entry-content>p>a>img", ".page-link-box a").then(srcs => {
+            srcs = srcs.map(e => e.replace("pic.4khd.com", "img.4khd.com"));
+            thumbnailSrcArray = srcs.map(e => e.replace(/\?w=\d+/, "?w=100"));
+            let bigSrcArray = srcs.map(e => e.replace(/\/w\d+-rw\//, "/w2500-h2500-rw/").replace(/\?w=\d+/, ""));
             if (fn.lh === "www.4khd.com") {
                 if (bigSrcArray[0].startsWith("https://img.4khd.com")) {
                     let host = new URL(bigSrcArray[0]).host;
@@ -6423,7 +6425,7 @@ a:has(>div>div>img),
                 let newImgOrigin = "https://i0.wp.com/img.4khd.com";
                 return bigSrcArray.map(src => src.replace(oldImgOrigin, newImgOrigin));
             }
-        },
+        }),
         button: [4],
         insertImg: [
             [".page-link-box,.wp-block-post-content>*:last-child,#khd", 1, "#basicExample,.wp-block-image,.entry-content>p:not(#FullPictureLoadEnd),.page-link-box"], 2
