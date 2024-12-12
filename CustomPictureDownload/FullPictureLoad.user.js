ab4c7daa879b4dc8c412e955915e90735f88b740
skofkyo <kyo7350201@gmail.com>
2024-12-12 20:01:24 +0800

    Update FullPictureLoad.user.js

--- CustomPictureDownload/FullPictureLoad.user.js
+++ CustomPictureDownload/FullPictureLoad.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load - FancyboxV5
 // @name:zh-CN         图片全载-FancyboxV5
 // @name:zh-TW         圖片全載-FancyboxV5
-// @version            2.11.50
+// @version            2.11.51
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully loaded images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
@@ -12690,15 +12690,18 @@ div[class*='backdrop-show'] {
             if (fn.lh === "nhentai.net") {
                 let image_domain;
                 let srcs = _unsafeWindow._gallery.images.pages.map((e, i) => `https://image_domain/galleries/${_unsafeWindow._gallery.media_id}/${i + 1}.${fn.ex(e.t)}`);
-                const hostArray = ["i.nhentai.net", "i1.nhentai.net", "i2.nhentai.net", "i3.nhentai.net", "i4.nhentai.net", "i5.nhentai.net", "i6.nhentai.net", "i7.nhentai.net"];
+                const hostArray = ["i.nhentai.net", "i5.nhentai.net", "i6.nhentai.net", "i7.nhentai.net", "i1.nhentai.net", "i2.nhentai.net", "i3.nhentai.net", "i4.nhentai.net"];
+                //fn.showMsg(displayLanguage.str_56, 0);
                 for (let host of hostArray.reverse()) {
+                    fn.showMsg(displayLanguage.str_56 + "_" + host, 0);
                     let src = srcs[0].replace("image_domain", host);
-                    let obj = await fn.checkImgStatus(src);
-                    if (obj.ok) {
+                    let status = await fn.xhrHEAD(src).then(res => res.status);
+                    if (status == 200) {
                         image_domain = host;
                         break;
                     }
                 }
+                fn.hideMsg();
                 return srcs.map(e => e.replace("image_domain", image_domain));
             } else if (fn.lh === "nyahentai.red") {
                 fn.showMsg(displayLanguage.str_05, 0);
@@ -12762,15 +12765,18 @@ div[class*='backdrop-show'] {
             } = _unsafeWindow;
             let image_domain;
             let srcs = _unsafeWindow._gallery.images.pages.map((e, i) => `https://image_domain/galleries/${_unsafeWindow._gallery.media_id}/${i + 1}.${fn.ex(e.t)}`);
-            const hostArray = ["i.nhentai.net", "i1.nhentai.net", "i2.nhentai.net", "i3.nhentai.net", "i4.nhentai.net", "i5.nhentai.net", "i6.nhentai.net", "i7.nhentai.net"];
+            const hostArray = ["i.nhentai.net", "i5.nhentai.net", "i6.nhentai.net", "i7.nhentai.net", "i1.nhentai.net", "i2.nhentai.net", "i3.nhentai.net", "i4.nhentai.net"];
+            //fn.showMsg(displayLanguage.str_56, 0);
             for (let host of hostArray.reverse()) {
+                fn.showMsg(displayLanguage.str_56 + "_" + host, 0);
                 let src = srcs[0].replace("image_domain", host);
-                let obj = await fn.checkImgStatus(src);
-                if (obj.ok) {
+                let status = await fn.xhrHEAD(src).then(res => res.status);
+                if (status == 200) {
                     image_domain = host;
                     break;
                 }
             }
+            fn.hideMsg();
             return srcs.map(e => e.replace("image_domain", image_domain));
         },
         button: [4],
@@ -14990,7 +14996,6 @@ div[class*='backdrop-show'] {
             e: [".header>.header-con>.logo", ".manga-page,.main-picture"],
             p: /^\/([\w-]+\/)?(h|hentai|cos|webtoon|western)\/\d+\.html$/
         },
-        //imgs: () => fn.getImg(".manga-page img,.main-picture img", fn.gt("#pages>*:last-child", 2), 5),
         imgs: async () => {
             await fn.getNP(".manga-page img,.main-picture img", "#pages span+a:not(.a1)", null, "#pages", 200);
             return fn.gae(".manga-page img,.main-picture img");
@@ -15011,7 +15016,6 @@ div[class*='backdrop-show'] {
             p: /^\/\w+\/\d+\.html$/,
             d: "m"
         },
-        //imgs: () => fn.getImg(".main-picture img", fn.gt("//a[text()='下一页'][@class='a1']", 2), 5),
         imgs: async () => {
             await fn.getNP(".main-picture", "#pages span+a:not(.a1)", null, "#pages", 200);
             return fn.gae(".main-picture img");
@@ -25880,13 +25884,16 @@ if ("xx" in window) {
                         }
                     }
                 }
-                let backgroundImage = getComputedStyle(ele).getPropertyValue("background-image");
-                if (backgroundImage != "none" && backgroundImage?.startsWith("url")) {
-                    let imgSrc = backgroundImage.slice(5, -2).trim();
-                    if (!!imgSrc) {
-                        return {
-                            ok: true,
-                            src: imgSrc
+
+                if (ele.tagName !== "IMG") {
+                    let backgroundImage = getComputedStyle(ele).getPropertyValue("background-image");
+                    if (backgroundImage != "none" && backgroundImage?.startsWith("url")) {
+                        let imgSrc = backgroundImage.slice(5, -2).trim();
+                        if (!!imgSrc) {
+                            return {
+                                ok: true,
+                                src: imgSrc
+                            }
                         }
                     }
                 }
