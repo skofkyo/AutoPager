373ee1fecbb9dfc7b48f4f2b2085be0a7e3a50dc
skofkyo <kyo7350201@gmail.com>
2024-12-14 22:20:47 +0800

    Update FullPictureLoad.user.js

--- CustomPictureDownload/FullPictureLoad.user.js
+++ CustomPictureDownload/FullPictureLoad.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load - FancyboxV5
 // @name:zh-CN         图片全载-FancyboxV5
 // @name:zh-TW         圖片全載-FancyboxV5
-// @version            2.11.52
+// @version            2.11.53
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully loaded images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
@@ -5202,7 +5202,10 @@ div[class*='backdrop-show'] {
             ],
             e: ["img.block", "//div[img[@title]]", "#main>h1,header>h1"]
         },
-        imgs: () => fn.getImg("img.block", fn.gt("a[rel=next]", 2) || 1),
+        imgs: async () => {
+            let srcs = await fn.getImg("img.block", fn.gt("a[rel=next]", 2) || 1);
+            return srcs.map(e => e.replace("teleimgs.pages.dev", "imgfiles.pages.dev"));
+        },
         button: [4],
         insertImg: ["//div[img[@title]]", 2],
         next: "//span[contains(text(),'上一篇')]/following-sibling::a[1]",
