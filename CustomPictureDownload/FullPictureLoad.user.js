0dbdb2a2262b4556336b77224ef3ba2d78660159
skofkyo <kyo7350201@gmail.com>
2024-12-29 16:19:03 +0800

    Update FullPictureLoad.user.js

--- CustomPictureDownload/FullPictureLoad.user.js
+++ CustomPictureDownload/FullPictureLoad.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load - FancyboxV5
 // @name:zh-CN         图片全载-FancyboxV5
 // @name:zh-TW         圖片全載-FancyboxV5
-// @version            2.12.6
+// @version            2.12.7
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully loaded images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
@@ -25397,10 +25397,10 @@ if ("xx" in window) {
                 galleryMenu: {
                     horizontal: hasTouchEvent ? "水平模式" : "水平模式 (5,B,R)",
                     webtoon: hasTouchEvent ? "條漫模式" : "條漫模式 (4,+,-)",
-                    rtl: hasTouchEvent ? "右至左模式" : "右至左模式 (3,R)",
-                    small: hasTouchEvent ? "小圖像模式" : "小圖像模式 (2,R)",
+                    rtl: hasTouchEvent ? "右至左模式" : "右至左模式 (3,B,R)",
+                    small: hasTouchEvent ? "小圖像模式" : "小圖像模式 (2,B,R)",
                     single: hasTouchEvent ? "單圖像模式" : "單圖像模式 (1)",
-                    default: hasTouchEvent ? "預設模式" : "預設模式 (0,R)",
+                    default: hasTouchEvent ? "預設模式" : "預設模式 (0,B,R)",
                 },
                 FancyboxWheel: {
                     z: "圖片縮放",
@@ -25621,10 +25621,10 @@ if ("xx" in window) {
                 galleryMenu: {
                     horizontal: hasTouchEvent ? "水平模式" : "水平模式 (5,B,R)",
                     webtoon: hasTouchEvent ? "条漫模式" : "条漫模式 (4,+,-)",
-                    rtl: hasTouchEvent ? "右至左模式" : "右至左模式 (3,R)",
-                    small: hasTouchEvent ? "小图像模式" : "小图像模式 (2,R)",
+                    rtl: hasTouchEvent ? "右至左模式" : "右至左模式 (3,B,R)",
+                    small: hasTouchEvent ? "小图像模式" : "小图像模式 (2,B,R)",
                     single: hasTouchEvent ? "单图像模式" : "单图像模式 (1)",
-                    default: hasTouchEvent ? "默认模式" : "默认模式 (0,R)",
+                    default: hasTouchEvent ? "默认模式" : "默认模式 (0,B,R)",
                 },
                 FancyboxWheel: {
                     z: "图片缩放",
@@ -25842,10 +25842,10 @@ if ("xx" in window) {
                 galleryMenu: {
                     horizontal: hasTouchEvent ? "Horizontal" : "Horizontal (5,B,R)",
                     webtoon: hasTouchEvent ? "Webtoon" : "Webtoon (4,+,-)",
-                    rtl: hasTouchEvent ? "Right To Left" : "Right To Left (3,R)",
-                    small: hasTouchEvent ? "Small Image" : "Small Image (2,R)",
+                    rtl: hasTouchEvent ? "Right To Left" : "Right To Left (3,B,R)",
+                    small: hasTouchEvent ? "Small Image" : "Small Image (2,B,R)",
                     single: hasTouchEvent ? "Single Image" : "Single Image (1)",
-                    default: hasTouchEvent ? "Default" : "Default (0,R)",
+                    default: hasTouchEvent ? "Default" : "Default (0,B,R)",
                 },
                 FancyboxWheel: {
                     z: "zoom",
@@ -31003,11 +31003,11 @@ body {
     font-weight: 500;
     font-size: 14px;
     color: #000000;
-    width: ${hasTouchEvent ? "102px" : "132px"};
+    width: ${hasTouchEvent ? "102px" : "144px"};
     height: auto;
     padding: 5px 5px 2px 5px;
     position: fixed;
-    left: ${hasTouchEvent ? "0px" : "-138px"};
+    left: ${hasTouchEvent ? "0px" : "-150px"};
     bottom: 0px;
     border: #ccc 1px solid;
     border-radius: 3px;
@@ -31018,7 +31018,7 @@ body {
     }
 }
 .FixedMenuitem {
-    width: ${hasTouchEvent ? "90px" : "120px"};
+    width: ${hasTouchEvent ? "90px" : "132px"};
     height: 24px;
     line-height: 24px;
     overflow: hidden;
@@ -31405,7 +31405,7 @@ document.addEventListener("keydown", event => {
             }
         }
     }
-    if ((event.code === "KeyB" || event.key === "b" || event.key === "B") && config.ViewMode == 5) {
+    if ((event.code === "KeyB" || event.key === "b" || event.key === "B") && [0, 2, 3, 5].some(m => config.ViewMode == m)) {
         imgs.forEach(img => {
             if (img.classList.contains("no_r_l_border")) {
                 img.classList.remove("no_r_l_border");
@@ -32038,7 +32038,7 @@ if (config.ViewMode == 1) {
                         return setTimeout(() => (location.href = nextLink), 500);
                     } else if (event.deltaY > 0 && imgViewIndex <= imgs.length - 1) {
                         imgViewIndex++;
-                        if (imgViewIndex > imgs.length - 1 && loopView != 1) {
+                        if (imgViewIndex > imgs.length - 1 && loopView != 1 && (!next && config.ViewMode != 5)) {
                             imgViewIndex = imgs.length - 1;
                             return;
                         }
@@ -32221,7 +32221,7 @@ if (config.ViewMode == 1) {
                     }
                 }
             }
-            if ((event.code === "KeyB" || event.key === "b" || event.key === "B") && config.ViewMode == 5) {
+            if ((event.code === "KeyB" || event.key === "b" || event.key === "B") && [0, 2, 3, 5].some(m => config.ViewMode == m)) {
                 imgs.forEach(img => {
                     if (img.classList.contains("no_r_l_border")) {
                         img.classList.remove("no_r_l_border");
@@ -32281,7 +32281,7 @@ if (config.ViewMode == 1) {
                 if (config.ViewMode == 5 && (event.code === "ArrowRight" || event.key === "ArrowRight")) return;
                 event.preventDefault();
                 imgViewIndex++;
-                if (imgViewIndex > imgs.length - 1 && loopView != 1) {
+                if (imgViewIndex > imgs.length - 1 && loopView != 1 && (!next && config.ViewMode != 5)) {
                     imgViewIndex = imgs.length - 1;
                     return;
                 }
@@ -32341,11 +32341,11 @@ p#imgBox {
     font-weight: 500;
     font-size: 14px;
     color: #000000;
-    width: 132px;
+    width: 144px;
     height: auto;
     padding: 5px 5px 2px 5px;
     position: fixed;
-    left: -138px;
+    left: -150px;
     bottom: 0px;
     border: #ccc 1px solid;
     border-radius: 3px;
@@ -32356,7 +32356,7 @@ p#imgBox {
     }
 }
 .FixedMenuitem {
-    width: 120px;
+    width: 132px;
     height: 24px;
     line-height: 24px;
     overflow: hidden;
@@ -32374,7 +32374,7 @@ p#imgBox {
     user-select: none;
 }
 #MenuJumpItem {
-    width: 130px;
+    width: 142px;
     padding: 0px;
 }
 .FixedMenuitem.active {
@@ -33107,7 +33107,7 @@ img.horizontal {
                         return setTimeout(() => (location.href = nextLink), 500);
                     } else if (event.deltaY > 0 && imgViewIndex <= imgs.length - 1) {
                         imgViewIndex++;
-                        if (imgViewIndex > imgs.length - 1 && loopView != 1) {
+                        if (imgViewIndex > imgs.length - 1 && loopView != 1 && (!next && config.ViewMode != 5)) {
                             imgViewIndex = imgs.length - 1;
                             return;
                         }
@@ -33290,7 +33290,7 @@ img.horizontal {
                     }
                 }
             }
-            if ((event.code === "KeyB" || event.key === "b" || event.key === "B") && config.ViewMode == 5) {
+            if ((event.code === "KeyB" || event.key === "b" || event.key === "B") && [0, 2, 3, 5].some(m => config.ViewMode == m)) {
                 imgs.forEach(img => {
                     if (img.classList.contains("no_r_l_border")) {
                         img.classList.remove("no_r_l_border");
@@ -33350,7 +33350,7 @@ img.horizontal {
                 if (config.ViewMode == 5 && (event.code === "ArrowRight" || event.key === "ArrowRight")) return;
                 event.preventDefault();
                 imgViewIndex++;
-                if (imgViewIndex > imgs.length - 1 && loopView != 1) {
+                if (imgViewIndex > imgs.length - 1 && loopView != 1 && (!next && config.ViewMode != 5)) {
                     imgViewIndex = imgs.length - 1;
                     return;
                 }
@@ -33408,11 +33408,11 @@ p#imgBox {
     font-weight: 500;
     font-size: 14px;
     color: #000000;
-    width: 132px;
+    width: 144px;
     height: auto;
     padding: 5px 5px 2px 5px;
     position: fixed;
-    left: -138px;
+    left: -150px;
     bottom: 0px;
     border: #ccc 1px solid;
     border-radius: 3px;
@@ -33423,7 +33423,7 @@ p#imgBox {
     }
 }
 .FixedMenuitem {
-    width: 120px;
+    width: 132px;
     height: 24px;
     line-height: 24px;
     overflow: hidden;
@@ -33441,7 +33441,7 @@ p#imgBox {
     user-select: none;
 }
 #MenuJumpItem {
-    width: 130px;
+    width: 142px;
     padding: 0px;
 }
 .FixedMenuitem.active {
