daec759ac3885739f48073f19025512d971c7766
skofkyo <kyo7350201@gmail.com>
2025-01-05 23:29:12 +0800

    Update FullPictureLoad.user.js

--- CustomPictureDownload/FullPictureLoad.user.js
+++ CustomPictureDownload/FullPictureLoad.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load - FancyboxV5
 // @name:zh-CN         图片全载-FancyboxV5
 // @name:zh-TW         圖片全載-FancyboxV5
-// @version            2.12.14
+// @version            2.12.15
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully loaded images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
@@ -985,9 +985,12 @@
         hide: ".center:has(>.dibu1),.center:has(>.dibu2)",
         category: "nsfw1"
     }, {
-        name: "高清图片吧M",
-        host: ["m.pic88.cc"],
-        reg: /^https?:\/\/m\.pic88\.cc\/\w+\/\d+\/\d+\.html$/,
+        name: "高清图片吧M/美女写真网M",
+        host: ["m.pic88.cc", "m.ku138.cc"],
+        reg: [
+            /^https?:\/\/m\.pic88\.cc\/\w+\/\d+\/\d+\.html$/,
+            /^https?:\/\/m\.ku138\.cc\/\w+\/\d+\/\d+\.html$/
+        ],
         box: [".PsBox", 2],
         imgs: ".ArticleImageBox>img",
         button: [4],
@@ -5899,6 +5902,7 @@
         imgs: ".thecontent a,.content-inner>p>a",
         button: [4],
         insertImg: ["//p[a[img]]", 2],
+        go: 1,
         customTitle: () => fn.dt({
             s: "h1",
             d: "nudecosplay.biz"
@@ -7249,8 +7253,10 @@
         category: "nsfw2"
     }, {
         name: "裏ピク",
-        host: ["www.urapic.com"],
-        reg: /^https?:\/\/www\.urapic\.com\/blog-entry-\d+\.html$/,
+        url: {
+            h: "www.urapic.com",
+            p: "/blog-entry-"
+        },
         imgs: "//div[@class='entry-body']//a[img[@title]] | //div[@class='entry_body']//a[img[@title]]",
         customTitle: () => fn.gt(".entry-title,.entry_title>h1").replace(/[ｗ]+$/, ""),
         setFancybox: true,
@@ -8261,7 +8267,7 @@
         name: "Models Vibe",
         host: ["www.modelsvibe.com"],
         reg: /^https?:\/\/www\.modelsvibe\.com\/[^/]+\/$/,
-        include: ".td-post-content img",
+        include: ".td-post-content img,.td-post-content .page-nav",
         init: () => {
             let ele = fn.ge("//p[br and not(contains(text(),'[ad_1]'))]");
             if (!!ele) {
@@ -8279,6 +8285,11 @@
                     insertBefore(te, e);
                 }
             }
+            fn.gae(".td-post-content .tdb-block-inner p").forEach(p => {
+                if (!fn.ge("img", p) && !p.innerText.includes("[ad_1]")) {
+                    tempEles.push(p);
+                }
+            });
         },
         imgs: () => {
             if (fn.ge(".page-nav")) {
@@ -8292,6 +8303,7 @@
         },
         button: [4],
         insertImg: [".td-post-content .tdb-block-inner", 2],
+        insertImgAF: (parent) => parent.firstChild.before(...tempEles),
         go: 1,
         customTitle: "h1.tdb-title-text",
         css: ".tdb_header_menu .tdb-menu .tdb-mega-menu-inactive,.tdb_header_menu .tdb-menu .tdb-menu-item-inactive{pointer-events:auto!important}.tdb_header_menu .tdb-menu .tdb-mega-menu-inactive > ul,.tdb_header_menu .tdb-menu .tdb-menu-item-inactive>ul{visibility:unset!important;opacity:1!important}.tdb_header_menu .tdb-normal-menu ul .tdb-menu-item{list-style-type:auto!important}",
@@ -8658,9 +8670,9 @@
         customTitle: ".entry-title",
         category: "nsfw1"
     }, {
-        name: "500 Brothers",
-        host: ["500brothersfun.blogspot.com"],
-        reg: /^https?:\/\/500brothersfun\.blogspot\.com\/\d+\/\d+\/[^\.]+\.html/,
+        name: "500 Brothers/Safebooru",
+        host: ["500brothersfun.blogspot.com", "safebooru.blogspot.com"],
+        reg: /^https?:\/\/(500brothersfun|safebooru)\.blogspot\.com\/\d+\/\d+\/[^\.]+\.html/,
         box: [".separator", 1],
         imgs: () => fn.gau(".separator>a").map(u => u.replace("/s1600/", "/s16000/")),
         button: [4],
