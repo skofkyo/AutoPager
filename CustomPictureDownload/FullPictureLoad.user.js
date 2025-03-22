65ae77053e2e53954069fe508501fd5490186f0e
skofkyo <kyo7350201@gmail.com>
2025-03-22 15:59:41 +0800

    Update FullPictureLoad.user.js

--- CustomPictureDownload/FullPictureLoad.user.js
+++ CustomPictureDownload/FullPictureLoad.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load
 // @name:zh-CN         图片全载Next
 // @name:zh-TW         圖片全載Next
-// @version            2025.3.23.16
+// @version            2025.3.22.16
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully load all images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
