bf4532e82d1e71e5c0e40647b7f2e6fb4501bd5c
skofkyo <kyo7350201@gmail.com>
2025-04-08 11:07:44 +0800

    Update FullPictureLoad_Github.user.js

--- CustomPictureDownload/FullPictureLoad_Github.user.js
+++ CustomPictureDownload/FullPictureLoad_Github.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load
 // @name:zh-CN         图片全载Next
 // @name:zh-TW         圖片全載Next
-// @version            2025.4.8
+// @version            2025.4.8.11
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully load all images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
@@ -21652,7 +21652,7 @@
             fn.showMsg(DL.str_05, 0);
             let code = fn.gst("load_image");
             let cid = Number(code.match(/\d+/));
-            return fn.fetchDoc(`/${fn.generateRandomString(30, 1)}.iog?cid=${cid}`).then(dom => [...dom.images]);
+            return fn.fetchDoc(`/${fn.generateRandomString(30, 1)}.iog?cid=${cid}`).then(dom => fn.gae("img[alt^=Page]", dom));
         },
         button: [4],
         insertImg: ["#FullPictureLoadMainImgBox", 2],
@@ -21671,9 +21671,9 @@
         }),
         category: "comic"
     }, {
-        name: "MANGA1000",
+        name: "RawKuro",
         url: {
-            h: ["manga1000.top", "kakuyomu.in", "mangakoma01.net"],
+            h: ["rawkuro.net", "manga1000.top", "kakuyomu.in", "mangakoma01.net"],
             st: "CHAPTER_ID"
         },
         box: ["#chapterContent", 2],
@@ -21688,15 +21688,19 @@
                 },
                 "body": null,
                 "method": "POST"
-            }).then(res => res.json()).then(json => [...fn.doc(json.html).images].map(e => e.src));
+            }).then(res => res.json()).then(json => {
+                let dom = fn.doc(json.html);
+                let divs = fn.gae(".separator", dom).sort((a, b) => a.dataset.index - b.dataset.index);
+                return divs.map(e => e.firstElementChild.href).filter(e => !e.includes("rawwkuro.jpg"));
+            });
         },
         button: [4],
         insertImg: ["#FullPictureLoadMainImgBox", 2],
         insertImgAF: () => fn.hideEle("#chapterContent"),
         endColor: "white",
         autoDownload: [0],
-        next: "a.nextBtn[href^='/']",
-        prev: "a.prevBtn[href^='/']",
+        next: "a.nextBtn[href*='/']",
+        prev: "a.prevBtn[href*='/']",
         customTitle: "main h1",
         category: "comic"
     }, {
