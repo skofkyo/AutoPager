e50210b519cbb246a9575031c6c11a0961795d88
skofkyo <kyo7350201@gmail.com>
2025-04-13 06:28:04 +0800

    Update FullPictureLoad_Github.user.js

--- CustomPictureDownload/FullPictureLoad_Github.user.js
+++ CustomPictureDownload/FullPictureLoad_Github.user.js
@@ -3,7 +3,7 @@
 // @name:en            Full Picture Load
 // @name:zh-CN         图片全载Next
 // @name:zh-TW         圖片全載Next
-// @version            2025.4.12
+// @version            2025.4.13
 // @description        支持寫真、H漫、漫畫的網站1000+，圖片全量加載，簡易的看圖功能，漫畫無限滾動閱讀模式，下載壓縮打包，如有下一頁元素可自動化下載。
 // @description:en     supports 1,000+ websites for photos, h-comics, and comics, fully load all images, simple image viewing function, comic infinite scroll read mode, and compressed and packaged downloads.
 // @description:zh-CN  支持写真、H漫、漫画的网站1000+，图片全量加载，简易的看图功能，漫画无限滚动阅读模式，下载压缩打包，如有下一页元素可自动化下载。
@@ -937,7 +937,7 @@
         host: ["www.mmxxdd.com"],
         link: "https://www.mmxxdd.com/website.html",
         url: {
-            e: ".logo img[alt=梦想岛]",
+            e: [".logo img[alt=梦想岛]", ".gallerypic img"],
             p: "/gallery/"
         },
         test: async (max) => {
@@ -1749,6 +1749,7 @@
             history: 0
         },
         openInNewTab: ".post-list-item a:not([target=_blank])",
+        topButton: true,
         category: "autoPager"
     }, {
         name: "8E资源站 歸檔自動翻頁",
@@ -1828,6 +1829,7 @@
             history: 0
         },
         openInNewTab: ".post-list-item a:not([target=_blank])",
+        topButton: true,
         category: "autoPager"
     }, {
         name: "8E资源站 搜索自動翻頁",
@@ -1867,6 +1869,7 @@
             pageNum: () => currentPageNum
         },
         openInNewTab: ".post-list-item a:not([target=_blank])",
+        topButton: true,
         category: "autoPager"
     }, {
         name: "夜社资源",
@@ -27369,7 +27372,20 @@ if ("xx" in window) {
     }
 
     function getType(object) {
-        return Object.prototype.toString.call(object).replace("[object ", "").replace("]", "");
+        try {
+            return Object.prototype.toString.call(object)?.replace("[object ", "")?.replace("]", "");
+        } catch {
+            return undefined;
+        }
+    }
+
+    function addElement(node, tag, attrs) {
+        const elem = document.createElement(tag);
+        Object.assign(elem, {
+            ...attrs
+        });
+        node.appendChild(elem);
+        return elem;
     }
 
     const hasTouchEvent = ("ontouchstart" in _unsafeWindow);
@@ -27416,7 +27432,13 @@ if ("xx" in window) {
     const _GM_registerMenuCommand = (() => isFn(GM_registerMenuCommand) ? GM_registerMenuCommand : GM.registerMenuCommand)();
     const _GM_unregisterMenuCommand = (() => isFn(GM_unregisterMenuCommand) ? GM_unregisterMenuCommand : GM.unregisterMenuCommand)();
     const _GM_getResourceText = (() => isFn(GM_getResourceText) ? GM_getResourceText : GM.getResourceText)();
-    const _GM_addElement = (() => isFn(GM_addElement) ? GM_addElement : GM.addElement)();
+    const _GM_addElement = (() => {
+        try {
+            return GM_addElement;
+        } catch {
+            return addElement;
+        }
+    })();
 
     const UI_zIndex = Number(_GM_getValue("UI_zIndex", 2147483647));
 
